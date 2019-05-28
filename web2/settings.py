2d1a1291bc9dc1fdb727832a9a5eb8e148aefb03
Guilherme Rodrigues <36749657+guilhermetr22@users.noreply.github.com>
2019-05-28 20:25:21 -0300

    Update settings.py

--- web2/settings.py
+++ web2/settings.py
@@ -144,3 +144,8 @@ LOGOUT_REDIRECT_URL = 'login'
 # Third party apps configuration
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
